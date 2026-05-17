6cdffabfe51834af437414f2bda49387f7c85849
arroz097 <luizgabrielmatera@hotmail.com>
2026-05-17 13:42:13 -0300

    fix math lib name conflict
    Signed-off-by: arroz097 <luizgabrielmatera@hotmail.com>

--- libs/math.lua
+++ libs/math.lua
@@ -1,21 +1,21 @@
-local math = {}
+local m = {}
 
-function math.clamp(min, n, max)
+function m.clamp(min, n, max)
     return math.min(math.max(n, min), max)
 end
 
 -- interpolates a value
-function math.lerp(a, b, t)
+function m.lerp(a, b, t)
     return a + (b - a) * t
 end
 
-function math.sign(n)
+function m.sign(n)
     return n > 0 and 1 or (n < 0 and -1 or 0)
 end
 
 -- math.floor basicamente
-function math.round(n)
+function m.round(n)
     return math.floor(n + 0.5)
 end
 
-return math
+return m
